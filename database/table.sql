eb94e61d1e03410b9d7f7ab902fff8365d39f43c
Amol jadhav <124095673+QuntamCoder@users.noreply.github.com>
2024-02-13 14:43:04 +0530

    signup page and login page added

--- database/table.sql
+++ database/table.sql
@@ -1,1 +1,12 @@
+//database name
 CREATE DATABASE IF NOT EXISTS bank;
+
+//employee signup table
+CREATE TABLE Employee_Signup (
+    emp_id INT PRIMARY KEY,
+    emp_name VARCHAR(255),
+    emp_desg VARCHAR(255),
+    user_name VARCHAR(255) UNIQUE,
+    pass VARCHAR(255)
+);
+
